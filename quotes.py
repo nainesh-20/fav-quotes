c69143b17e735fb89aad8e248583957135001256
Nainesh Rathod <naineshrathod20@gmail.com>
2020-12-12 09:47:48 +0530

    v2.1

--- quotes.py
+++ quotes.py
@@ -1,8 +1,6 @@
 from flask import Flask ,render_template,request,redirect,url_for
 from flask_sqlalchemy import SQLAlchemy
 
-from flask_script import Manager
-from flask_migrate import Migrate,MigrateCommand
 
 app = Flask(__name__)
 
@@ -11,12 +9,6 @@ app.config['SQLALCHEMY_DATABASE_URI'] = 'postgres://jswieqwiecefli:cb3c7eb28d43a
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS']= False
 
 db = SQLAlchemy(app)
-
-migrate = Migrate()
-migrate.init_app(app, db)
-manager = Manager(app)
-
-manager.add_command('db', MigrateCommand)
 
 class Favquotes(db.Model):
     id = db.Column(db.Integer,primary_key=True)
@@ -43,6 +35,3 @@ def process():
     db.session.add(quotedata)
     db.session.commit()
     return redirect(url_for('index'))
-
-if __name__ == "__main__":
-    manager.run()
